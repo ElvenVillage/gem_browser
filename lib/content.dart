ea1b367414338a8cc8459006b247d33d0f9795a8
Denis Volkov <dvolkovdd@gmail.com>
2021-09-20 12:46:04 +0300

    20.09

--- lib/content.dart
+++ lib/content.dart
@@ -1,8 +1,9 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:strelka/net.dart';
 
-final tabProvider = StateNotifierProvider<TabNotifier, AsyncValue<Page>>(
-    (ref) => TabNotifier(ref));
+final tabProvider =
+    StateNotifierProvider.family<TabNotifier, AsyncValue<GeminiPage>, int>(
+        (ref, idx) => TabNotifier(ref));
 
 abstract class ContentElement {}
 
@@ -17,26 +18,48 @@ class Link implements ContentElement {
   Link({required this.address, required this.link});
 }
 
-class Page {
-  final List<ContentElement> content;
-  Page(this.content);
+class HeaderElement implements ContentElement {
+  final String content;
+  final int level;
 
-  factory Page.parse(String rawText) {
-    final lines = rawText.split('\n');
+  HeaderElement({required this.content, required this.level});
+}
+
+class GeminiPage {
+  final String url;
+  final List<ContentElement> content;
+  GeminiPage({required this.content, required this.url});
+
+  GeminiPage.empty()
+      : content = const [],
+        url = '';
+
+  factory GeminiPage.parse(List<String> lines, String url) {
     final content = <ContentElement>[];
 
     for (final line in lines) {
       if (line.startsWith('=>')) {
-        final linkComponents = line.split(' ');
+        final linkComponents = line.split(RegExp('\\s+'));
         content.add(Link(
             address: linkComponents[1],
             link: linkComponents.sublist(2).join(' ')));
+        continue;
       } else {
+        if (line.startsWith('##')) {
+          content
+              .add(HeaderElement(content: line.replaceAll('#', ''), level: 2));
+          continue;
+        }
+        if (line.startsWith('#')) {
+          content
+              .add(HeaderElement(content: line.replaceAll('#', ''), level: 1));
+          continue;
+        }
         content.add(Paragraph(line));
       }
     }
 
-    return Page(content);
+    return GeminiPage(content: content, url: url);
   }
 }
 
@@ -47,33 +70,54 @@ class HistoryEntry {
   HistoryEntry(this.time, this.address);
 }
 
-class TabNotifier extends StateNotifier<AsyncValue<Page>> {
+class TabNotifier extends StateNotifier<AsyncValue<GeminiPage>> {
   final List<HistoryEntry> _history = [];
   final ProviderReference _ref;
 
-  TabNotifier(this._ref) : super(AsyncValue.loading());
+  TabNotifier(this._ref) : super(AsyncValue.data(GeminiPage.empty()));
 
-  void goToUri(String url) async {
+  void goToUri(String url, {bool ignoreHistory = false}) async {
     state = AsyncValue.loading();
 
     var uri = Uri.parse(url);
-    if (!uri.hasScheme) uri = uri.replace(scheme: 'gemini://');
-    if (!uri.hasAuthority) uri = uri.replace(host: _history.last.address.host);
+    if (!uri.hasAuthority) {
+      var lastAddress = _history.last.address.toString();
+      if (!url.startsWith('/')) uri = Uri.parse('/$url');
+      if (lastAddress.endsWith('/'))
+        lastAddress = lastAddress.substring(0, lastAddress.length - 1);
+      uri = Uri.parse((lastAddress + url));
+    }
+    if (!uri.hasScheme) uri = uri.replace(scheme: 'gemini');
 
     try {
       final content = await (_ref.watch(geminiProvider(uri).future));
-      state = AsyncValue.data(Page.parse(content));
-      _history.add(HistoryEntry(DateTime.now(), uri));
+      final lines = content.split('\n');
+      final header = lines.first;
+      final statusCode = int.parse(header.substring(0, 3));
+      if (statusCode >= 20 && statusCode < 30) {
+        state = AsyncValue.data(
+            GeminiPage.parse(lines.skip(1).toList(), uri.toString()));
+        if (!ignoreHistory) _history.add(HistoryEntry(DateTime.now(), uri));
+      }
+      if (statusCode >= 30 && statusCode < 40) {
+        final elements = header.split(RegExp('\\s+'));
+        goToUri(elements[1].trim());
+      }
+      if (statusCode >= 40) {
+        state = AsyncValue.error('Status Code $statusCode');
+        if (!ignoreHistory) _history.add(HistoryEntry(DateTime.now(), uri));
+      }
     } catch (e) {
-      state = AsyncValue.error('a');
+      state = AsyncValue.error('Error loading page');
+      if (!ignoreHistory) _history.add(HistoryEntry(DateTime.now(), uri));
     }
   }
 
   bool goBack() {
-    if (_history.isEmpty) return true;
+    if (_history.isEmpty || _history.length == 1) return true;
     final prev = _history[_history.length - 2];
     _history.removeLast();
-    goToUri(prev.address.toString());
+    goToUri(prev.address.toString(), ignoreHistory: true);
     return false;
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -4,66 +4,303 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:strelka/content.dart';
 
 void main() {
-  runApp(ProviderScope(child: Strelka()));
+  runApp(ProviderScope(
+      child: MaterialApp(
+    routes: {'/list': (_) => TabListScreen(), '/tabs': (_) => Strelka()},
+    initialRoute: '/tabs',
+  )));
 }
 
-class Strelka extends StatelessWidget {
+class TabsState {
+  final int currentId;
+  final int numOfTabs;
+  final List<String> tabNames;
+
+  TabsState(
+      {required this.currentId,
+      required this.numOfTabs,
+      required this.controller,
+      required this.tabNames});
+
+  TabsState.empty()
+      : controller = PageController(),
+        currentId = 0,
+        tabNames = ['StartPage'],
+        numOfTabs = 1;
+
+  TabsState copyWith({int? currentId, int? numOfTabs, List<String>? tabNames}) {
+    return TabsState(
+        controller: this.controller,
+        currentId: currentId ?? this.currentId,
+        tabNames: tabNames ?? this.tabNames,
+        numOfTabs: numOfTabs ?? this.numOfTabs);
+  }
+
+  final PageController controller;
+}
+
+class TabListNotifier extends StateNotifier<TabsState> {
+  final ProviderReference _ref;
+  TabListNotifier(this._ref) : super(TabsState.empty());
+
+  void selectTab(int tab) {
+    if (tab >= 0 && tab < state.numOfTabs) {
+      state.controller.jumpToPage(tab);
+      state = state.copyWith(currentId: tab);
+    }
+  }
+
+  void createTab({String? url, bool silent = false}) {
+    state = state.copyWith(
+        currentId: silent ? null : state.numOfTabs + 1,
+        numOfTabs: state.numOfTabs + 1,
+        tabNames: [...state.tabNames, 'Blank Page']);
+    if (url != null)
+      _ref.watch(tabProvider(state.currentId).notifier).goToUri(url);
+  }
+
+  void setTabName({required int idx, required String name}) {
+    final newTabNames = state.tabNames;
+    newTabNames[idx] = name;
+    state = state.copyWith(tabNames: newTabNames);
+  }
+
+  void removeTab(int idx) {
+    final newTabsNames = state.tabNames..removeAt(idx);
+    state =
+        state.copyWith(numOfTabs: state.numOfTabs - 1, tabNames: newTabsNames);
+  }
+}
+
+final tabListProvider = StateNotifierProvider<TabListNotifier, TabsState>(
+    (ref) => TabListNotifier(ref));
+
+class TabListScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: SafeArea(
-          child: Scaffold(
-              body: WillPopScope(
-        onWillPop: () async {
-          return context.read(tabProvider.notifier).goBack();
-        },
-        child: Container(
-            color: Colors.black,
-            child: Column(
-              children: [
-                SizedBox(
-                  height: 60,
-                  child: TextField(
-                    onSubmitted: (url) {
-                      context.read(tabProvider.notifier).goToUri(url);
-                    },
-                    style: TextStyle(color: Colors.white),
-                  ),
-                ),
-                Expanded(child: SingleChildScrollView(child: Content())),
-              ],
-            )),
-      ))),
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Consumer(
+            builder: (context, watch, child) {
+              final tabsState = watch(tabListProvider);
+              final listOfTabs = watch(tabListProvider).tabNames;
+              return Column(
+                children: [
+                  Text('GEMPODS',
+                      style: TextStyle(
+                          fontFamily: 'monospace',
+                          color: Colors.white,
+                          fontSize: 23)),
+                  for (var i = 0; i < tabsState.numOfTabs; i++)
+                    Dismissible(
+                      onDismissed: (_) {
+                        watch(tabListProvider.notifier).removeTab(i);
+                      },
+                      key: UniqueKey(),
+                      child: MaterialButton(
+                          onPressed: () {
+                            watch(tabListProvider.notifier).selectTab(i);
+                            Navigator.of(context).pop();
+                          },
+                          child: Text(
+                            listOfTabs[i],
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontFamily: 'monospace',
+                                decoration: i == tabsState.currentId
+                                    ? TextDecoration.underline
+                                    : null),
+                          )),
+                    ),
+                  MaterialButton(
+                      onPressed: () {
+                        watch(tabListProvider.notifier).createTab(
+                            url: 'gemini://geminispace.info', silent: true);
+                        Navigator.of(context).pop();
+                      },
+                      child: Text(
+                        '+',
+                        style: TextStyle(color: Colors.white),
+                      )),
+                ],
+              );
+            },
+          ),
+        ),
+      ),
     );
   }
 }
 
-class Content extends ConsumerWidget {
+class Strelka extends ConsumerWidget {
   @override
   Widget build(BuildContext context, watch) {
-    final content = watch(tabProvider);
+    final tabsCount = watch(tabListProvider).numOfTabs;
+    final controller = watch(tabListProvider).controller;
+    return PageView.builder(
+      itemBuilder: (context, index) {
+        return Tab(
+          index,
+          initialUrl: 'gemini://geminispace.info',
+        );
+      },
+      itemCount: tabsCount,
+      controller: controller,
+    );
+  }
+}
+
+class Tab extends StatefulWidget {
+  Tab(this._idx, {this.initialUrl}) : super(key: ValueKey(_idx));
+  final int _idx;
+  final String? initialUrl;
+
+  @override
+  _TabState createState() => _TabState();
+}
+
+class _TabState extends State<Tab> with AutomaticKeepAliveClientMixin {
+  final _controller = TextEditingController();
+
+  @override
+  void initState() {
+    if (widget.initialUrl != null)
+      context
+          .read(tabProvider(widget._idx).notifier)
+          .goToUri(widget.initialUrl!);
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    super.build(context);
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: WillPopScope(
+        onWillPop: () async {
+          return context.read(tabProvider(widget._idx).notifier).goBack();
+        },
+        child: SafeArea(
+          child: CustomScrollView(
+            slivers: [
+              SliverAppBar(
+                expandedHeight: 60,
+                floating: true,
+                backgroundColor: Colors.black,
+                automaticallyImplyLeading: false,
+                title: ProviderListener(
+                  provider: tabProvider(widget._idx),
+                  onChange: (context, AsyncValue<GeminiPage> value) {
+                    value.whenData((value) {
+                      _controller.text = value.url;
+                      context
+                          .read(tabListProvider.notifier)
+                          .setTabName(idx: widget._idx, name: value.url);
+                    });
+                  },
+                  child: Row(
+                    children: [
+                      Flexible(
+                        flex: 6,
+                        child: Container(
+                          padding: const EdgeInsets.only(left: 8),
+                          child: SizedBox(
+                            height: 60,
+                            child: TextField(
+                              controller: _controller,
+                              style: const TextStyle(
+                                  color: Colors.white, fontFamily: 'monospace'),
+                              onSubmitted: (url) {
+                                context
+                                    .read(tabProvider(widget._idx).notifier)
+                                    .goToUri(url);
+                              },
+                            ),
+                          ),
+                        ),
+                      ),
+                      Flexible(
+                        child: IconButton(
+                          icon: Icon(
+                            Icons.grid_3x3,
+                            color: Colors.white,
+                          ),
+                          onPressed: () {
+                            Navigator.of(context).pushNamed('/list');
+                          },
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+              SliverToBoxAdapter(child: Content(widget._idx))
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  bool get wantKeepAlive => true;
+}
+
+class Content extends ConsumerWidget {
+  final textStyle = TextStyle(color: Colors.white, fontFamily: 'monospace');
+  final int _idx;
+  Content(this._idx);
+  @override
+  Widget build(BuildContext context, watch) {
+    final content = watch(tabProvider(_idx));
     return content.when(
         data: (data) {
-          return SelectableText.rich(TextSpan(
-              children: data.content.map((e) {
-            if (e is Paragraph)
+          return Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: SelectableText.rich(TextSpan(
+                children: data.content.map((e) {
+              if (e is Paragraph)
+                return TextSpan(
+                    text: '  ' + e.content + '\n', style: textStyle);
+              if (e is HeaderElement)
+                return TextSpan(
+                    text: (e.level == 1 ? '# ' : '## ') + e.content,
+                    style: TextStyle(
+                        fontSize: e.level == 1 ? 24 : 18,
+                        color: Colors.white,
+                        fontFamily: 'monospace',
+                        fontWeight: FontWeight.bold));
+              if (e is Link)
+                return TextSpan(
+                    text: '\n' + e.link + '\n',
+                    style: TextStyle(
+                        decoration: TextDecoration.underline,
+                        fontFamily: 'monospace',
+                        color: Colors.white),
+                    recognizer: TapGestureRecognizer()
+                      ..onTap = () {
+                        context
+                            .read(tabProvider(_idx).notifier)
+                            .goToUri(e.address);
+                      });
               return TextSpan(
-                  text: e.content + '\n',
-                  style: TextStyle(color: Colors.white));
-            if (e is Link)
-              return TextSpan(
-                  text: e.link + '\n',
-                  style: TextStyle(
-                      color: Colors.white,
-                      decoration: TextDecoration.underline),
-                  recognizer: TapGestureRecognizer()
-                    ..onTap = () {
-                      context.read(tabProvider.notifier).goToUri(e.address);
-                    });
-            return TextSpan(text: '', style: TextStyle(color: Colors.white));
-          }).toList()));
+                text: '',
+              );
+            }).toList())),
+          );
         },
-        loading: () => CircularProgressIndicator(),
+        loading: () => Center(
+            child: SizedBox(
+                height: 50, width: 50, child: CircularProgressIndicator())),
         error: (err, stackTrace) => Text(err.toString()));
   }
 }
